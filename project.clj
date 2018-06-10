1d1d538601f115d1756117050b868afef3d26837
Luke De Feo <luke.defeo@just-eat.com>
2018-06-10 09:40:13 +0100

    removed content script

--- project.clj
+++ project.clj
@@ -14,8 +14,7 @@
             [lein-cooper "1.2.2"]]
 
   :source-paths ["src/background"
-                 "src/popup"
-                 "src/content_script"]
+                 "src/popup"]
 
   :clean-targets ^{:protect false} ["target"
                                     "resources/unpacked/compiled"
@@ -45,18 +44,7 @@
                                            :main          chromex-sample.popup
                                            :optimizations :none
                                            :source-map    true}}}}}
-             :unpacked-content-script
-             {:cljsbuild {:builds
-                          {:content-script
-                           {:source-paths ["src/content_script"]
-                            :compiler     {:output-to     "resources/unpacked/compiled/content-script/main.js"
-                                           :output-dir    "resources/unpacked/compiled/content-script"
-                                           :asset-path    "compiled/content-script"
-                                           :main          chromex-sample.content-script
-                                           ;:optimizations :whitespace                                                        ; content scripts cannot do eval / load script dynamically
-                                           :optimizations :advanced                                                           ; let's use advanced build with pseudo-names for now, there seems to be a bug in deps ordering under :whitespace mode
-                                           :pseudo-names  true
-                                           :pretty-print  true}}}}}
+
              :checkouts
              ; DON'T FORGET TO UPDATE scripts/ensure-checkouts.sh
              {:cljsbuild {:builds
@@ -66,12 +54,7 @@
                            :popup      {:source-paths ["checkouts/cljs-devtools/src/lib"
                                                        "checkouts/chromex/src/lib"
                                                        "checkouts/chromex/src/exts"]}}}}
-             :checkouts-content-script
-             ; DON'T FORGET TO UPDATE scripts/ensure-checkouts.sh
-             {:cljsbuild {:builds
-                          {:content-script {:source-paths ["checkouts/cljs-devtools/src/lib"
-                                                           "checkouts/chromex/src/lib"
-                                                           "checkouts/chromex/src/exts"]}}}}
+
 
              :figwheel
              {:figwheel {:server-port    6888
@@ -104,25 +87,15 @@
                                            :asset-path    "compiled/popup"
                                            :main          chromex-sample.popup
                                            :optimizations :advanced
-                                           :elide-asserts true}}
-                           :content-script
-                           {:source-paths ["src/content_script"]
-                            :compiler     {:output-to     "resources/release/compiled/content-script.js"
-                                           :output-dir    "resources/release/compiled/content-script"
-                                           :asset-path    "compiled/content-script"
-                                           :main          chromex-sample.content-script
-                                           :optimizations :advanced
                                            :elide-asserts true}}}}}}
 
-  :aliases {"dev-build"       ["with-profile" "+unpacked,+unpacked-content-script,+checkouts,+checkouts-content-script" "cljsbuild" "once"]
+  :aliases {"dev-build"       ["with-profile" "+unpacked,+unpacked-content-script,+checkouts" "cljsbuild" "once"]
             "fig"             ["with-profile" "+unpacked,+figwheel" "figwheel" "background" "popup"]
-            "content"         ["with-profile" "+unpacked-content-script" "cljsbuild" "auto" "content-script"]
             "fig-dev-no-repl" ["with-profile" "+unpacked,+figwheel,+disable-figwheel-repl,+checkouts" "figwheel" "background" "popup"]
-            "content-dev"     ["with-profile" "+unpacked-content-script,+checkouts-content-script" "cljsbuild" "auto"]
             "devel"           ["with-profile" "+cooper" "do"                                                                  ; for mac only
                                ["shell" "scripts/ensure-checkouts.sh"]
                                ["cooper"]]
             "release"         ["with-profile" "+release" "do"
                                ["clean"]
-                               ["cljsbuild" "once" "background" "popup" "content-script"]]
+                               ["cljsbuild" "once" "background" "popup"]]
             "package"         ["shell" "scripts/package.sh"]})
