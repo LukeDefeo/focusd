441ab855ca4566ca528f15d9e13a06d013a6314c
Luke De Feo <luke.defeo@just-eat.com>
2018-06-12 20:53:25 +0100

    fixed problems

--- src/background/chromex_sample/background/core.cljs
+++ src/background/chromex_sample/background/core.cljs
@@ -55,48 +55,55 @@
 ; -- main event loop --------------------------------------------------------------------------------------------------------
 
 (defn url->context-id [url]
-  (if (str/starts-with? url "https://www.bbc.co.uk")
+  (if (or (str/starts-with? url "https://www.bbc.co.uk") (str/starts-with? url "http://testcontext.org"))
     :news
-    :other))
+    nil))
 
 
-(defn create-window [context-id]
+(defn <create-window [context-id]
   (go
-    (let [_ (swap! window-state assoc context-id :temp)
-          id (-> (<! (windows/create (clj->js {:url ["https://www.bbcd.co.uk" "https://www.blah.co.uk"]})))
-                 (js->clj :keywordize-keys true)
-                 first
-                 :id)]                                      ;;for some reason its a vector
+    (let [
+          ;_ (swap! window-state assoc context-id :temp)
+          {:keys [id tabs] :as window} (first (js->clj (<! (windows/create)) :keywordize-keys true))]                                      ;;for some reason its a vector
       (swap! window-state assoc context-id id)
-      (println "created window for context-id " context-id "with id" id)
+      (println " created window for context-id " context-id "with id" id "and rest" window)
       (println "new state " @window-state)
-      id)))
+      window)))
 
 
-(defn context-id->window-id [context]
+(defn <context-id->window-id [context]
   (go
     (if-let [window-id (get @window-state context)]
-      window-id
-      (<! (create-window context)))))
+      [(first (js->clj (<! (windows/get window-id)))) :existing]
+      [(<! (<create-window context)) :new])))
 
 
-(defn process-updated-tab! [[event-id [num status event]]]
-  ;(println "got 2 " num)
-  ;(println "got 3 " first)
-  ;(println "got 4 " second)
+(defn <move-tab-to-context [{:keys [url id] :as tab} context-id]
+  (println "move tab to context for url " url " tab id " id "context" context-id)
+  (go
+    (let [[dest-window state] (<! (<context-id->window-id context-id))
+          current-window (first (js->clj (<! (windows/get-last-focused))))
+          _ (println "current window" current-window " dest window " dest-window)]
 
-  ;(println "got updated event " event)
+      (when (not= (:id dest-window) (:id current-window))
+        (println "tab with " url "will be moved to " (:id dest-window))
+        (let [moved (<! (tabs/move id (clj->js {:windowId (:id dest-window) :index -1})))
+              _ (when (= :new state) (<! (tabs/remove (-> dest-window :tabs first :id))))
+              focused (<! (windows/update (:id dest-window) (clj->js {:focused true})))
+              active (<! (tabs/update id (clj->js {:active true})))]))
+      )))
+
+(def updated-tabs (chan 100))
+
+(defn process-updated-tab! [[_ _ event]]
   (go
     (let [{:keys [url] :as event} (js->clj event :keywordize-keys true)
-          dest-window-id (->
-                           (url->context-id url)
-                           context-id->window-id
-                           <!)]
-      (println "tab with " url "should go in " dest-window-id)))
+          _ (println "processing updated tab" url)
 
-  ;(println "url mm?" )
-  ;(println "url" (aget event "url"))
-  )
+          context-id (url->context-id url)]
+      (if context-id
+        (<! (<move-tab-to-context event context-id))
+        (println "no context for" url)))))
 
 (defn window-id->context-id [window-id]
   (get (set/map-invert @window-state) window-id))
@@ -111,20 +118,20 @@
   (println "state now" @window-state))
 
 (defn process-chrome-event [event-num event]
-  ;(log (gstring/format "BACKGROUND: got chrome event (%05d)" event-num) event)
-  (let [[event-id event-args] event]
-    (case event-id
-      ::runtime/on-connect (apply handle-client-connection! event-args)
-      ::tabs/on-created (tell-clients-about-new-tab!)
-      ::windows/on-removed (handle-closed-window! (first event-args))
-      ::tabs/on-updated (process-updated-tab! event)
-      nil)))
+  (go
+    (let [[event-id event-args] event]
+      (case event-id
+        ::runtime/on-connect (apply handle-client-connection! event-args)
+        ::tabs/on-created (tell-clients-about-new-tab!)
+        ::windows/on-removed (handle-closed-window! (first event-args))
+        ::tabs/on-updated (<! (process-updated-tab!  event-args)) ;maybe should be in vector ot prevent nils   ; (process-updated-tab! event-args)
+        nil))))
 
 (defn run-chrome-event-loop! [chrome-event-channel]
   (log "BACKGROUND: starting main event loop...")
   (go-loop [event-num 1]
            (when-some [event (<! chrome-event-channel)]
-             (process-chrome-event event-num event)
+             (<! (process-chrome-event event-num event))
              (recur (inc event-num)))
            (log "BACKGROUND: leaving main event loop")))
 
@@ -139,5 +146,6 @@
 
 (defn init! []
   (log "BACKGROUND: init")
+  ;(process-updated-tab-channel!)
   (test-storage!)
   (boot-chrome-event-loop!))
