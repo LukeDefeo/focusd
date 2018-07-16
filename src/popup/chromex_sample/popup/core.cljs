168d20f354713b91f6b6361071949e6e6186679d
Luke De Feo <luke.defeo@just-eat.com>
2018-07-16 13:30:10 +0100

    added swticher ui

--- src/popup/chromex_sample/popup/core.cljs
+++ src/popup/chromex_sample/popup/core.cljs
@@ -7,9 +7,12 @@
             [chromex.ext.runtime :as runtime :refer-macros [connect]]
             [re-com.buttons :refer [button md-circle-icon-button]]
             [reagent.core :as r]
+            [chromex-sample.popup.page :as page]
             [chromex-sample.shared.util :refer [js->clj-keyed js->clj-keyed-first]]
             [chromex-sample.shared.communication :refer [parse-client-message send-message!]]
-            [re-com.util :refer [get-element-by-id item-for-id]]))
+            [re-com.util :refer [get-element-by-id item-for-id]]
+            [chromex.ext.windows :as windows]
+            ))
 
 ; -- a message loop ---------------------------------------------------------------------------------------------------------
 
@@ -18,8 +21,15 @@
 (defmulti handle-client-message (fn [[sender message-type payload]] [sender message-type]))
 
 (defmethod handle-client-message [:background :state-update] [[_ _ payload]]
-  (println "got new rules from back ground" payload)
+  (println "got new rules 2 from background" payload)
   (reset! window-state payload)
+  (page/mount payload (fn [context]
+                        (if-let [id (:window-id context)]
+                          (do
+                            (println "activating window " id)
+                            (windows/update id (clj->js {:focused true})))
+                          (println "no window id for context"))
+                        ))
   )
 
 (defmethod handle-client-message [:background :ping] [[sender _ message]]
@@ -27,7 +37,6 @@
 
 (defmethod handle-client-message :default [[sender message-type payload]]
   (println "unknown message from " sender " with type " message-type))
-
 
 (defn run-message-loop! [message-channel]
   (log "POPUP: starting message loop...")
@@ -58,4 +67,5 @@
 (defn init! []
   (log "POPUP:  init")
   (connect-to-background-page!)
-  (mount))
+  ;(mount)
+  )
---
+++ src/popup/chromex_sample/popup/page.cljs
@@ -0,0 +1,75 @@
+(ns chromex-sample.popup.page
+  (:require
+    [reagent.core :as reagent]
+    [reagent.ratom :refer-macros [reaction]]
+    [re-com.text :refer [title label]]
+    [re-com.buttons :refer [button md-circle-icon-button]]
+    [clojure.string :as str]
+    [re-com.box :refer [h-box v-box box gap]]
+    [re-com.util :refer [get-element-by-id]]))
+
+
+(defn wrap-in-div [highlighted? click-Handler comp]
+  [:div {:style    {:background-color (if highlighted? "red" "white")}
+         :on-click click-Handler} comp])
+
+(def down-arrow 40)
+(def up-arrow 38)
+(def enter 13)
+
+
+(defn cycle-up [total-items cur]
+  (let [max-idx (dec total-items)]
+    (cond
+      (= cur 0) max-idx
+      :else (dec cur))))
+
+
+(defn cycle-down [total-items cur]
+  (let [max-idx (dec total-items)]
+    (cond
+      (= cur max-idx) 0
+      :else (inc cur))))
+
+
+(defn list-comp [items render-fn selected-fn]
+  (let [state (reagent/atom {:index 0})]
+    (fn [items render-fn selected-fn]
+      (let [{:keys [index]} @state]
+        [v-box
+         :attr {:id          "list-box"
+                :tab-index   0
+                :on-key-down (fn [key]
+                               (let [code (-> key .-keyCode)]
+                                 (condp = code
+                                   down-arrow (swap! state update :index (partial cycle-down (count items)))
+                                   up-arrow (swap! state update :index (partial cycle-up (count items)))
+                                   enter (selected-fn (nth items index))
+                                   nil)))}
+         :children
+         (map-indexed
+           (fn [cur-idx item]
+             (let [handler (fn [_]
+                             ;update highlight
+                             (swap! state assoc :index cur-idx)
+                             (selected-fn item))]
+               (->> item render-fn (wrap-in-div (= index cur-idx) handler))))
+           items)]))))
+
+
+(defn render-context [{:keys [name]}]
+  [:div {:style {:width "300px"}} name])
+
+(defn main [contexts]
+  [list-comp
+   contexts
+   (fn [{:keys [name]}] [:div name])
+   (fn [item] (println "selected" item))])
+
+(defn mount
+  [contexts selected-fn]
+  (reagent/render
+    [list-comp contexts render-context selected-fn] (get-element-by-id "app"))
+  (.focus (get-element-by-id "list-box")))
+
+
